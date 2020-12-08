53e1f5c5f3339d7166cbdd5a903c31f4b2130e32
Matt Harrington <MattHarrington@users.noreply.github.com>
2020-12-08 13:03:13 -0800

    Refactored day 6 part2() to avoid repeating logic

--- day6/src/lib.cpp
+++ day6/src/lib.cpp
@@ -35,6 +35,7 @@ size_t part1(const std::vector<std::string>& customs_forms)
         }
         std::for_each(line.cbegin(), line.cend(), [&customs_form](const char c) {customs_form[c]++; });
     }
+    // See part2() for a way to handle last form besides repeating logic
     questions_answered_yes.push_back(customs_form.size()); // Handle last form in file, since no blank line follows it
     return std::accumulate(questions_answered_yes.begin(), questions_answered_yes.end(), 0ULL);
 }
@@ -45,9 +46,13 @@ size_t part2(const std::vector<std::string>& customs_forms)
     std::vector<size_t> questions_everyone_answered_yes;
     auto people_on_form{ 0 };
 
-    for (const auto& line : customs_forms)
+    for (auto it{ customs_forms.begin() }; true; ++it)
     {
-        if (line.empty()) // Empty lines separate forms
+        people_on_form++;
+        std::for_each((*it).cbegin(), (*it).cend(), [&customs_form](const char c) {customs_form[c]++; });
+
+        // Check if we're at the end of a customs form
+        if (std::next(it) == customs_forms.end() || (*std::next(it)).empty())
         {
             // When value == number of people on form, everyone answered yes
             const auto num_all_answered_yes{ std::count_if(customs_form.cbegin(), customs_form.cend(),
@@ -55,20 +60,15 @@ size_t part2(const std::vector<std::string>& customs_forms)
 
             questions_everyone_answered_yes.push_back(num_all_answered_yes);
 
+            if (std::next(it) == customs_forms.end()) break; // All forms have been processed, so break
+
             // Reset, because we're about to process a new form
             customs_form.clear();
             people_on_form = 0;
-            continue;
+
+            ++it; // Skip over the next line since it's blank.  Start processing a new form next iteration.
         }
-
-        people_on_form++;
-        std::for_each(line.cbegin(), line.cend(), [&customs_form](const char c) {customs_form[c]++; });
     }
-
-    // Handle last form in file, since no blank line follows it
-    const auto num_all_answered_yes{ std::count_if(customs_form.cbegin(), customs_form.cend(),
-        [people_on_form](const std::pair<const char, int> elem) {return elem.second == people_on_form; }) };
-    questions_everyone_answered_yes.push_back(num_all_answered_yes);
 
     return std::accumulate(questions_everyone_answered_yes.begin(), questions_everyone_answered_yes.end(), 0ULL);
 }
